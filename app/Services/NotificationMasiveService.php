93dca4c697268a12f1c15a331d866d340b862953
Gustavo <gusta.acp@gmail.com>
2025-09-14 16:48:10 -0400

    subiendo cambios en url

--- app/Services/NotificationMasiveService.php
+++ app/Services/NotificationMasiveService.php
@@ -43,7 +43,7 @@ class NotificationMasiveService
                     $params = array(
                         'token' => config('parameters.TOKEN'),
                         'to' => $array[$i]['phone'],
-                        'image' => 'https://integracorp.dev.tudrgroup.com/storage/'. $infoArray['file'],
+                        'image' => config('parameters.PUBLIC_URL') . '/' . $infoArray['file'],
                         'caption' => $body
                     );
                     $curl = curl_init();
