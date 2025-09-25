69d670f131a1c69199c23b466e9f215c4bea5932
Gustavo <gusta.acp@gmail.com>
2025-09-25 16:57:10 -0400

    subiendo cambio para asociacion de data capemiac

--- app/Filament/Marketing/Resources/Capemiacs/Tables/CapemiacsTable.php
+++ app/Filament/Marketing/Resources/Capemiacs/Tables/CapemiacsTable.php
@@ -2,12 +2,20 @@
 
 namespace App\Filament\Marketing\Resources\Capemiacs\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
+use Filament\Tables\Table;
+use App\Models\DataNotification;
+use App\Models\MassNotification;
+use Filament\Actions\BulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Forms\Components\Select;
+use Filament\Actions\DeleteBulkAction;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
+use Filament\Notifications\Notification;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Tables\Columns\TextInputColumn;
+use Illuminate\Database\Eloquent\Collection;
 
 class CapemiacsTable
 {
@@ -21,13 +29,9 @@ class CapemiacsTable
                     ->searchable(),
                 TextColumn::make('rif')
                     ->searchable(),
-                TextColumn::make('telefonoUno')
+                TextInputColumn::make('telefonoUno')
                     ->searchable(),
-                TextColumn::make('telefonoDos')
-                    ->searchable(),
-                TextColumn::make('telefonoTres')
-                    ->searchable(),
-                TextColumn::make('email')
+                TextInputColumn::make('email')
                     ->label('Email address')
                     ->searchable(),
                 TextColumn::make('fecha_registro')
@@ -50,7 +54,44 @@ class CapemiacsTable
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
-                    DeleteBulkAction::make(),
+                    BulkAction::make('associateInfo')
+                        ->label('Asociar informacion')
+                        ->icon('heroicon-s-link')
+                        ->form([
+                            Fieldset::make('Asociar Información')
+                                ->columns(1)
+                                ->schema([
+                                    Select::make('mass_notification_id')
+                                        ->label('Asociar Notificación')
+                                        ->options(MassNotification::all()->pluck('title', 'id'))
+                                        ->required(),
+                                ])
+                        ])
+                        ->action(function (Collection $records, $data) {
+
+                            $info = $records->toArray();
+
+                            for ($i = 0; $i < count($info); $i++) {
+                                $dataInfo = new DataNotification();
+                                $dataInfo->mass_notification_id = $data['mass_notification_id'];
+                                $dataInfo->fullName             = $info[$i]['cliente'];
+                                $dataInfo->email                = $info[$i]['email'];
+                                $dataInfo->phone                = $info[$i]['telefonoUno'];
+                                $dataInfo->save();
+                            }
+
+                            Notification::make()
+                                ->title('Información asociada')
+                                ->body(count($info) . ' agencias asociados correctamente.')
+                                ->success()
+                                ->send();
+
+                            $id = $data['mass_notification_id'];
+
+                            return redirect()->route('filament.marketing.resources.mass-notifications.view', ['record' => $id]);
+                        })
+                        ->requiresConfirmation()
+                        ->color('primary'),
                 ]),
             ]);
     }
