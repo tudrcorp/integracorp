c192850bd75085c273b3eceaf54a36421d598f03
Gustavo <gusta.acp@gmail.com>
2026-01-20 09:46:27 -0400

    subiendo cambios para pruebas v2

--- resources/views/mails/cumpleanos-email.blade.php
+++ resources/views/mails/cumpleanos-email.blade.php
@@ -23,38 +23,6 @@
                         <td align="center" style="padding: 20px 10px;">
                             <img src="{{ config('parameters.PUBLIC_URL') . '/' . $file }}" style="display: block; width: 100%; max-width: 600px; height: auto;" alt="Felicidades">
                         </td>
-                        {{-- <td align="center" style="padding: 20px 0;">
-                            <!-- Contenedor con ancho máximo para asegurar proporcionalidad -->
-                            <div
-                                style="position: relative; width: 100%; max-width: 600px; margin: 0 auto; overflow: hidden; border-radius: 12px; shadow: 0 4px 15px rgba(0,0,0,0.1);">
-                        
-                                <!-- 1. IMAGEN DE FONDO (TARJETA) -->
-                                <img src="{{ config('parameters.PUBLIC_URL') . '/' . $file }}"
-                                    style="display: block; width: 100%; height: auto;" alt="Tarjeta de Cumpleaños">
-                        
-                                <!-- 2. CAPA DEL NOMBRE (FLOTANTE Y CURSIVA) -->
-                                <!-- 
-                                            Ajusta 'top' para subir o bajar el nombre.
-                                            'font-size: 5vw' hace que el texto sea proporcional al ancho de la pantalla (responsive).
-                                        -->
-                                <div style="position: absolute; top: 10%; left: 0; width: 100%; text-align: center; pointer-events: none;">
-                                    <span style="
-                                                font-family: 'Dancing Script', 'Brush Script MT', 'cursive'; 
-                                                font-size: 48px; 
-                                                font-size: 8vw; 
-                                                color: black; 
-                                                font-style: italic;
-                                                font-weight: normal;
-                                                text-shadow: 2px 2px 8px rgba(0,0,0,0.4);
-                                                display: inline-block;
-                                                padding: 0 10px;
-                                                line-height: 1.2;
-                                            ">
-                                        GUSTAVO CAMACHO
-                                    </span>
-                                </div>
-                            </div>
-                        </td> --}}
                     </tr>
                     <tr>
                         <td align="center" style="padding: 20px 10px;">
